8d91bbb1b78db90178a96057fa5e187809792cf8
TheAnh <theanh@TheAnh.local>
2019-06-11 14:00:23 +0700

    Fix bug

--- NoobTour/ViewController.swift
+++ NoobTour/ViewController.swift
@@ -12,34 +12,40 @@ import FBSDKLoginKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var loginButton:FBLoginButton!
+    @IBOutlet weak var loginButton: FBLoginButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loginButton.delegate = self
-    }
-    
-    @IBAction func loginWithFacebook() {
         loginButton.permissions = ["public_profile", "email"]
     }
     
-    func showToast(message : String) {
-        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
-        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
-        toastLabel.textColor = UIColor.white
-        toastLabel.textAlignment = .center;
-        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
-        toastLabel.text = message
-        toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds  =  true
-        self.view.addSubview(toastLabel)
-        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
-        }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
-        })
+    @IBAction func loginWithFacebook() {
+        let fbLoginManager = LoginManager()
+        fbLoginManager.logIn(permissions: ["public_profile","email"], from: self) { (result, error) -> Void in
+            if let error = error {
+                self.showDialogMessage(title: "Login failed", message: error.localizedDescription)
+                return
+            }
+            if (result?.isCancelled)!{
+                return
+            }
+            if let accessToken = AccessToken.current?.tokenString {
+                self.authFirebase(accessToken : accessToken)
+            }
+        }
+    }
+    
+    func authFirebase(accessToken : String) {
+        let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
+        Auth.auth().signIn(with: credential) { (authResult, error) in
+            if let error = error {
+                self.showDialogMessage(title: "Login failed", message: error.localizedDescription)
+                return
+            }
+            self.showDialogMessage(title: "Congrats", message: "Login successfully")
+        }
     }
     
     func showDialogMessage(title: String ,message : String) {
@@ -51,7 +57,6 @@ class ViewController: UIViewController {
 
 extension ViewController : LoginButtonDelegate {
     func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-        showToast(message: "log out successfully!")
     }
     
     func loginButton(_ loginButton: FBLoginButton, didCompleteWith result: LoginManagerLoginResult?, error: Error?) {
@@ -59,16 +64,12 @@ extension ViewController : LoginButtonDelegate {
             self.showDialogMessage(title: "Login failed", message: error.localizedDescription)
             return
         }
-        let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
-        Auth.auth().signIn(with: credential) { (authResult, error) in
-            if let error = error {
-                self.showDialogMessage(title: "Login failed", message: error.localizedDescription)
-                return
-            }
-            self.showDialogMessage(title: "Congrats", message: "Login successfully")
+        if (result?.isCancelled)!{
+            return
+        }
+        if let accessToken = AccessToken.current?.tokenString {
+            self.authFirebase(accessToken : accessToken)
         }
     }
 }
-
-
 
